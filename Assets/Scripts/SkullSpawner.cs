d276ae3fe8da17a644eb0a4c790b0bee92a9709e
Captain Ravenclaw <danieltellezgiron81@gmail.com>
2023-07-31 14:05:18 -0600

    Changes to be committed:     new file:   .DS_Store     new file:   Assets/.DS_Store     new file:   Assets/Images/.DS_Store     new file:   Assets/Images/game_ss.png     modified:   Assets/Prefabs/Skull Variant.prefab     modified:   Assets/Scripts/SkullSpawner.cs     modified:   ProjectSettings/TagManager.asset     modified:   README.md

--- Assets/Scripts/SkullSpawner.cs
+++ Assets/Scripts/SkullSpawner.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class BallSpawner : MonoBehaviour
 {
-    public GameObject basketballPrefab; // Drag and drop the basketball prefab to this variable in the Inspector.
-    public float spawnInterval = 2f; // Time interval between spawning basketballs.
-    public float upwardForce = 10f; // The force applied to make the basketball go upwards.
-    public float forwardForce = 5f; // The force applied to make the basketball move forward.
-    public float randomDeviation = 2f; // Random deviation for the direction of the basketball.
+    public GameObject basketballPrefab;
+    public float spawnInterval = 2f;
+    public float upwardForce = 10f;
+    public float forwardForce = 5f;
+    public float randomDeviation = 2f;
 
     private Coroutine spawnCoroutine;
 
@@ -28,18 +28,21 @@ public class BallSpawner : MonoBehaviour
     {
         while (true)
         {
-            // Create a new basketball instance at the spawner's position and rotation.
-            GameObject basketball = Instantiate(basketballPrefab, transform.position, Quaternion.identity);
+            if (GameObject.FindGameObjectsWithTag("ball").Length == 0)
+            {
+                // Create a new basketball instance at the spawner's position and rotation.
+                GameObject basketball = Instantiate(basketballPrefab, transform.position, Quaternion.identity);
 
-            // Get the rigidbody of the basketball.
-            Rigidbody rb = basketball.GetComponent<Rigidbody>();
+                // Get the rigidbody of the basketball.
+                Rigidbody rb = basketball.GetComponent<Rigidbody>();
 
-            // Calculate the direction vector with slight random deviation.
-            Vector3 direction = Quaternion.Euler(Random.Range(-randomDeviation, randomDeviation), Random.Range(-randomDeviation, randomDeviation), 0f) * Vector3.forward;
+                // Calculate the direction vector with slight random deviation.
+                Vector3 direction = Quaternion.Euler(Random.Range(-randomDeviation, randomDeviation), Random.Range(-randomDeviation, randomDeviation), 0f) * Vector3.forward;
 
-            // Apply the initial forces to the basketball.
-            rb.AddForce(direction.normalized * forwardForce, ForceMode.VelocityChange);
-            rb.AddForce(Vector3.up * upwardForce, ForceMode.VelocityChange);
+                // Apply the initial forces to the basketball.
+                rb.AddForce(direction.normalized * forwardForce, ForceMode.VelocityChange);
+                rb.AddForce(Vector3.up * upwardForce, ForceMode.VelocityChange);
+            }
 
             // Wait for the next spawn interval.
             yield return new WaitForSeconds(spawnInterval);
