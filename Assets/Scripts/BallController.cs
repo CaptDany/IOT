e1bcb290fc7b30a37db451b8d01d3d6b98dee4d4
Alan Barucq <barucq22@gmail.com>
2023-08-02 16:28:38 -0600

    Ball Spawn modified

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,20 +2,16 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public GameObject ballPrefab;  // Assign your Ball prefab in the inspector
-    public Transform startPoint; // Assign your start point in the inspector
-    public Transform endPoint; // Assign your end point in the inspector
+    public GameObject ballPrefab;
     private GameObject currentBall;
-    private void SpawnAndJumpBall()
+    public Vector3 position;
+
+    public void SpawnBallAtPosition()
     {
-        // If a ball already exists, we destroy it.
         if (currentBall != null)
         {
             Destroy(currentBall);
         }
-        // Instantiate new ball object at the start point position
-        currentBall = Instantiate(ballPrefab, startPoint.position, Quaternion.identity);
-        // Make your ball jump to the endPoint position
-        currentBall.GetComponent<Rigidbody>().AddForce((endPoint.position - startPoint.position) * 2, ForceMode.VelocityChange);
+        currentBall = Instantiate(ballPrefab, position, Quaternion.identity);
     }
 }
